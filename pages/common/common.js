d0c4cfafd7c852af2205e0a513ba298e6c8cf87e
Yinuo-M <mengyn1999@gmail.com>
2021-06-30 10:15:11 +0100

    Fix file links

--- pages/common/common.js
+++ pages/common/common.js
@@ -1,11 +1,14 @@
 import "focus-visible";
 import "../../stylesheets/common/common.scss";
 import smoothscroll from "smoothscroll-polyfill";
+import englishCV from "../../assets/CV - Wentao Li.pdf";
+import chineseCV from "../../assets/简历-李文韬-牛津大学-金融学.pdf";
+import courseMaterial from "../../assets/Introductory Econometrics.rar";
 smoothscroll.polyfill();
 
 //ANCHOR setting up HMR for the dev server.
 if (module.hot) {
-	module.hot.accept();
+  module.hot.accept();
 }
 
 //ANCHOR navigation
@@ -13,23 +16,23 @@ if (module.hot) {
 window.addEventListener("resize", updateMenu);
 
 function updateMenu() {
-	const menuButton = document.querySelector(".hamburger");
-	if (!menuButton) return;
+  const menuButton = document.querySelector(".hamburger");
+  if (!menuButton) return;
 
-	menuButton.addEventListener("click", toggleMenu);
+  menuButton.addEventListener("click", toggleMenu);
 }
 
 function toggleMenu() {
-	const menuButton = document.querySelector(".hamburger");
-	const menu = document.querySelector(".menu__list");
+  const menuButton = document.querySelector(".hamburger");
+  const menu = document.querySelector(".menu__list");
 
-	if (menu.classList.contains("menu__list--displayed")) {
-		menu.classList.remove("menu__list--displayed");
-		menuButton.setAttribute("aria-expanded", "false");
-	} else {
-		menu.classList.add("menu__list--displayed");
-		menuButton.setAttribute("aria-expanded", "true");
-	}
+  if (menu.classList.contains("menu__list--displayed")) {
+    menu.classList.remove("menu__list--displayed");
+    menuButton.setAttribute("aria-expanded", "false");
+  } else {
+    menu.classList.add("menu__list--displayed");
+    menuButton.setAttribute("aria-expanded", "true");
+  }
 }
 
 updateMenu();
@@ -38,14 +41,14 @@ updateMenu();
 window.addEventListener("resize", updateToTop);
 
 function updateToTop() {
-	const backButton = document.querySelector(".top-button");
-	if (!backButton) return;
+  const backButton = document.querySelector(".top-button");
+  if (!backButton) return;
 
-	backButton.addEventListener("click", backToTop);
+  backButton.addEventListener("click", backToTop);
 }
 
 function backToTop() {
-	window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
 }
 
 updateToTop();
--- webpack.common.js
+++ webpack.common.js
@@ -1,64 +1,64 @@
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: {
-		common: "./pages/common/common.js",
-		home: "./pages/home/index.js",
-		research: "./pages/research/research.js",
-		teaching: "./pages/teaching/teaching.js",
-		tutoring: "./pages/tutoring/tutoring.js",
-		ch: "./pages/common/ch.js",
-	},
-	target: "web",
-	devServer: {
-		hot: true,
-	},
-	module: {
-		rules: [
-			{ test: /\.html$/, use: ["html-loader"] },
-			{ test: /\.(jpe?g|gif|svg|png|ico|pdf|rar)$/, type: "asset/resource" },
-		],
-	},
-	plugins: [
-		new HTMLWebpackPlugin({
-			template: "./pages/home/index.html",
-			chunks: ["common", "home"],
-			filename: "index.html",
-		}),
-		new HTMLWebpackPlugin({
-			template: "./pages/home/index-ch.html",
-			chunks: ["common", "home", "ch"],
-			filename: "index-ch.html",
-		}),
-		new HTMLWebpackPlugin({
-			template: "./pages/research/research.html",
-			chunks: ["common", "research"],
-			filename: "research.html",
-		}),
-		new HTMLWebpackPlugin({
-			template: "./pages/research/research-ch.html",
-			chunks: ["common", "research", "ch"],
-			filename: "research-ch.html",
-		}),
-		new HTMLWebpackPlugin({
-			template: "./pages/teaching/teaching.html",
-			chunks: ["common", "teaching"],
-			filename: "teaching.html",
-		}),
-		new HTMLWebpackPlugin({
-			template: "./pages/teaching/teaching-ch.html",
-			chunks: ["common", "teaching", "ch"],
-			filename: "teaching-ch.html",
-		}),
-		new HTMLWebpackPlugin({
-			template: "./pages/tutoring/tutoring.html",
-			chunks: ["common", "tutoring"],
-			filename: "tutoring.html",
-		}),
-		new HTMLWebpackPlugin({
-			template: "./pages/tutoring/tutoring-ch.html",
-			chunks: ["common", "tutoring", "ch"],
-			filename: "tutoring-ch.html",
-		}),
-	],
+  entry: {
+    common: "./pages/common/common.js",
+    home: "./pages/home/index.js",
+    research: "./pages/research/research.js",
+    teaching: "./pages/teaching/teaching.js",
+    tutoring: "./pages/tutoring/tutoring.js",
+    ch: "./pages/common/ch.js",
+  },
+  target: "web",
+  devServer: {
+    hot: true,
+  },
+  module: {
+    rules: [
+      { test: /\.html$/, use: ["html-loader"] },
+      { test: /\.(jpe?g|gif|svg|png|ico|pdf|rar)$/, type: "asset/resource" },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./pages/home/index.html",
+      chunks: ["common", "home"],
+      filename: "index.html",
+    }),
+    new HTMLWebpackPlugin({
+      template: "./pages/home/index-ch.html",
+      chunks: ["common", "home", "ch"],
+      filename: "index-ch.html",
+    }),
+    new HTMLWebpackPlugin({
+      template: "./pages/research/research.html",
+      chunks: ["common", "research"],
+      filename: "research.html",
+    }),
+    new HTMLWebpackPlugin({
+      template: "./pages/research/research-ch.html",
+      chunks: ["common", "research", "ch"],
+      filename: "research-ch.html",
+    }),
+    new HTMLWebpackPlugin({
+      template: "./pages/teaching/teaching.html",
+      chunks: ["common", "teaching"],
+      filename: "teaching.html",
+    }),
+    new HTMLWebpackPlugin({
+      template: "./pages/teaching/teaching-ch.html",
+      chunks: ["common", "teaching", "ch"],
+      filename: "teaching-ch.html",
+    }),
+    new HTMLWebpackPlugin({
+      template: "./pages/tutoring/tutoring.html",
+      chunks: ["common", "tutoring"],
+      filename: "tutoring.html",
+    }),
+    new HTMLWebpackPlugin({
+      template: "./pages/tutoring/tutoring-ch.html",
+      chunks: ["common", "tutoring", "ch"],
+      filename: "tutoring-ch.html",
+    }),
+  ],
 };
